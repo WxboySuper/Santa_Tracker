86e2bb3f9a4b52ed811c446f75271a101425865d
WeatherboySuper <SuperPlays1020@gmail.com>
2024-12-24 03:40:40 -0600

    Fix: Remove unused import

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from utils.route_tracker import get_current_position, load_route, haversine_distance
+from utils.route_tracker import get_current_position, load_route
 
 app = Flask(__name__)
 
