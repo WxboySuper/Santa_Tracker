a6480e8c4677f9a14b220072964c3c714e5f1efa
Alex Miller <alex.miller.6464@gmail.com>
2026-08-04 00:06:21 -0500

    chore: include expectBeforeSendToDrop in expect-expect assertion helpers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -93,6 +93,7 @@ export default [
         assertFunctionNames: [
           'expect',
           'expectRegistryValidationError',
+          'expectBeforeSendToDrop',
           'expectBeforeSendToKeep',
           'expectCopyVerification',
           'expectCopyResult',
